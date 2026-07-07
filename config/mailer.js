14a2a0c942a3713ec0820de35c87c256ef8756ac
Maha Elsheikhe <mahaelshakhe@gmail.com>
2026-07-07 12:02:00 +0300

    push mailer

--- config/mailer.js
+++ config/mailer.js
@@ -1,6 +1,25 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
+
+// module.exports = transporter;
+
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// FIX: Force Node to prefer IPv4. This bypasses Render's IPv6 routing issues.
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
+  service: "gmail", // It's highly recommended to use the service flag for Gmail
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
